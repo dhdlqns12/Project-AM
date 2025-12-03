9e77780018a9daf2f5d184f95901dc52937e4c43
JeonGyuTae <115717251+JeonGyuTae@users.noreply.github.com>
2025-12-03 11:42:42 +0900

    feat: TextMoney Script 추가
    Stage1-1 씬에서는 Script 적용 해야 합니다!

---
+++ Assets/02_Scripts/UI/TextMoney.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextMoney : MonoBehaviour
+{
+    private StageManager stageManager;
+
+    private TextMeshProUGUI moneyText;
+
+    private void Awake()
+    {
+        stageManager = StageManager.Instance;
+
+        moneyText = GetComponent<TextMeshProUGUI>();
+
+        if (moneyText == null)
+        {
+            Debug.Log("moneyText가 연동되지 않음");
+        }
+    }
+
+    void Update()
+    {
+        UpdateMoney();
+    }
+
+    private void UpdateMoney()
+    {
+        moneyText.text = stageManager.Gold.ToString();
+    }
+}
