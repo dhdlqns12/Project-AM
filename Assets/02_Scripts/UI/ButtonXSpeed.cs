56dbc7ee0b622fab67c487bea5f47d5d0eb07093
JeonGyuTae <115717251+JeonGyuTae@users.noreply.github.com>
2025-12-03 15:13:50 +0900

    fix: 배속 기능 구현 + Timer 위치 이동, Timer 표기 자릿수 수정

---
+++ Assets/02_Scripts/UI/ButtonXSpeed.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ButtonXSpeed : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private TextMeshProUGUI buttonXSpeed;
+
+    private void Awake()
+    {
+        gameManager = GameManager.Instance;
+
+        buttonXSpeed = GetComponentInChildren<TextMeshProUGUI>();
+
+        if (buttonXSpeed == null)
+        {
+            Debug.LogWarning("배속 Text가 연동되지 않음");
+        }
+    }
+
+    public void UpdateXSpeed()
+    {
+        if (Time.timeScale == 1)
+        {
+            gameManager.AdjustTime(2);
+
+            buttonXSpeed.text = $"{Time.timeScale}";
+        }
+        else if (Time.timeScale == 2)
+        {
+            gameManager.AdjustTime(1);
+
+            buttonXSpeed.text = $"{Time.timeScale}";
+        }
+    }
+}
