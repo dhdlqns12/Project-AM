86e5aeaed9ecbf90d11aea297e76a1353c2307da
lachy75127873-oss <lachy75127873@gmail.com>
2025-12-03 12:48:10 +0900

    feat: 일정 킬 수 이상 달성 시 골드 지급 로직을 StageManager에 작성

--- Assets/02_Scripts/Manager/StageManager.cs
+++ Assets/02_Scripts/Manager/StageManager.cs
@@ -12,16 +12,31 @@ public class StageManager : Singleton<StageManager>
         set => gold = value;
     }
 
+    private int enemyKillCount;
+
+    private const int GOLD_PER_KILL = 10;
+
+    private const int BASE_KILL = 4;
+
+    public int EnemyKillCount
+    {
+        get => enemyKillCount;
+        set => enemyKillCount = value;
+    }
+
     protected override void Init()
     {
         Debug.Log("스테이지 매니저 초기화");
         ResetGold();
+        ResetKillCount();
     }
 
     // public void SetStartGold()
     // {
     //     gold = 1000;
     // }
+
+    #region 골드 관련 기본 로직
 
     public void ResetGold()
     {
@@ -39,6 +54,33 @@ public class StageManager : Singleton<StageManager>
         gold -= amount;
     }
     
-    //ui랑 연결 방식 고민
+    #endregion
+
+    #region 킬카운트 관련 로직
+
+    private void ResetKillCount()
+    {
+        enemyKillCount = 0;
+    }
     
+    public void PlusKillCount()
+    {
+        enemyKillCount++;
+        Debug.Log("현재 킬 수: "+enemyKillCount);
+        if (enemyKillCount >= BASE_KILL)
+        {
+            RewardKillCount();
+            enemyKillCount -= BASE_KILL;
+        }
+    }
+
+    private void RewardKillCount()
+    {
+        gold += GOLD_PER_KILL;
+    }
+    
+    #endregion
+    
+    //ui랑 연결 방식 고민
+    //enemy킬카운트 필드 가지고 퍼블릭으로, 리셋할 때마다 초기화시키고, n킬마다 골드지급하는 로직
 }
